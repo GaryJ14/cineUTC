b6288244dd0bc708088cb1731c59f7a83837d803
GARY ISMAEL JAMI CHUQUILLA <gary.jami6400@utc.edu.ec>
2024-08-01 10:26:03 -0500

    Nes medwarly

--- cineUTC/urls.py
+++ cineUTC/urls.py
@@ -36,5 +36,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER='gary,jami6400@utc.edu.ec'
+EMAIL_HOST_USER='gary.jami6400@utc.edu.ec'
 EMAIL_HOST_PASSWORD='cqjy kohf jnkb arga'
